36c36f958fd5a12799ad39d00678def14c719571
Dimopoulos Stavros tou Athanasiou <sdim@sdim.gr>
2020-02-06 15:46:51 +0200

    Initial Commit

---
+++ IOSND-VirtualTurist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSND-VirtualTurist
+//
+//  Created by Dimopoulos Stavros tou Athanasiou on 6/2/20.
+//  Copyright © 2020 Dimopoulos Stavros tou Athanasiou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
